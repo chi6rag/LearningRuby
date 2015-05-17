dec1227a989b0a8ce12ce84f030293437b243323
Chirag Aggarwal <chi6rag@gmail.com>
2015-05-17 18:34:45 +0530

    Solve 2 and 3

--- OOPS_Ruby_Tealeaf/cop_1/my_car.rb
+++ OOPS_Ruby_Tealeaf/cop_1/my_car.rb
@@ -1,5 +1,6 @@
 class MyCar
-	attr_accessor :year, :color, :model, :speed
+	attr_accessor :color, :speed
+  attr_reader :year, :model
   
   def initialize(year, color, model, speed=0)
   	@year = year
@@ -19,6 +20,10 @@ class MyCar
   def shut_down
   	self.speed = 0
   end
+
+  def spray_paint(color)
+    @color = color
+  end
 end
 
 c = MyCar.new(1994, 'red', 'abcman', 321)
@@ -30,3 +35,8 @@ puts c.brake
 puts c.brake
 puts c.shut_down
 puts c.accelerate
+c.color = 'blue'
+puts c.color
+puts c.year
+c.spray_paint('yellow')
+puts c.color
